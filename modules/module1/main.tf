133b8c5908576ed3f2944661268ffeb210b7a846
Karl Schriek <karl.schriek@bma.network>
2024-12-03 17:57:07 +0100

    add more outputs

--- modules/module1/main.tf
+++ modules/module1/main.tf
@@ -1,7 +1,19 @@
 resource random_pet this {}
+resource random_pet this2 {}
+
+resource random_pet this3 {}
 
 variable input1 {}
-
 output output1 {
+  value = random_pet.this.id
+}
+output module1_output1 {
+  value = random_pet.this.id
+}
+output module1_output2 {
+  value = random_pet.this.id
+}
+
+output module1_output3 {
   value = random_pet.this.id
 }
--- modules/module2/main.tf
+++ modules/module2/main.tf
@@ -2,6 +2,10 @@ resource random_pet this {}
 
 variable input2 {}
 
+variable module1_output2 {}
+
+variable module1_output3 {}
+
 output output2 {
   value = random_pet.this.id
 }
